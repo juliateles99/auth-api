1472da797b973b072cb07959b724e21f928f0574
juliateles99 <juliateles.github@gmail.com>
2025-05-05 21:21:50 -0300

    feat: adiciona entidades User e Role e enum RoleName

---
+++ src/main/java/com/julia/authapi/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.julia.authapi.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/julia/authapi/model/User.java
@@ -0,0 +1,34 @@
+package com.julia.authapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
